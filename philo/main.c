154cbbde5aec243eddb52bafaa901cec71eb8e33
luozguo <anitadou9@gmail.com>
2026-03-03 23:31:37 +0100

    mutex init tested.

--- philo/main.c
+++ philo/main.c
@@ -1,20 +1,6 @@
 #include "philo.h"
 
-
-int parse_args(t_control_philo0 *ctl, int argc, char **argv)
-{
-    if (check_args_legitimacy(argc, argv))
-    {
-        printf("this is not acceptible for me for a reason stated above.");
-        return 1;
-    }
-    load_ctl_from_args(ctl, argc, argv);
-    if (init_ctl(ctl))
-        return 1;
-    return (0);
-}
-
-/// args could be 5 - 6; must check the type
+// /// args could be 5 - 6; must check the type
 int main(int argc, char **argv)
 {
     t_control_philo0 ctl;
