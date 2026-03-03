c39e8e45af7a5f62f5592e08e75c40531ef5d471
luozguo <anitadou9@gmail.com>
2026-03-03 22:23:25 +0100

    pre-build for bonus

---
+++ philo_bonus/philo_bonus.h
@@ -0,0 +1,32 @@
+#ifndef PHILO_BONUS_H
+#define PHILO_BONUS_H
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stddef.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+// /* Allowed external functions (bonus) */
+// extern void	*memset(void *s, int c, size_t n);
+// extern int	printf(const char *format, ...);
+// extern void	*malloc(size_t size);
+// extern void	free(void *ptr);
+// extern ssize_t	write(int fd, const void *buf, size_t count);
+// extern pid_t	fork(void);
+// extern int	kill(pid_t pid, int sig);
+// extern void	exit(int status);
+// extern int	pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+// 				void *(*start_routine)(void *), void *arg);
+// extern int	pthread_detach(pthread_t thread);
+// extern int	pthread_join(pthread_t thread, void **retval);
+// extern int	usleep(useconds_t usec);
+// extern int	gettimeofday(struct timeval *tv, struct timezone *tz);
+// extern pid_t	waitpid(pid_t pid, int *wstatus, int options);
+// extern sem_t	*sem_open(const char *name, int oflag, ...);
+// extern int	sem_close(sem_t *sem);
+// extern int	sem_post(sem_t *sem);
+// extern int	sem_wait(sem_t *sem);
+// extern int	sem_unlink(const char *name);
+
+#endif
