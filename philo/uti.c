fa413c7aa015c8380f5a5abecbfd5151a0304a1e
luozguo <anitadou9@gmail.com>
2026-03-05 20:08:12 +0100

    restruc small funcs into utility throughout the project

--- philo/uti.c
+++ philo/uti.c
@@ -1,5 +1,16 @@
 #include "philo.h"
 
+// get current time and transform it to milliseconds. return -1 on error
+long get_current_time_in_ms(void)
+{
+    long now;
+    struct timeval tv;
+    if (gettimeofday(&tv, NULL) == -1)
+        return -1;
+    now = 1000 * tv.tv_sec + tv.tv_usec/1000;
+    return now;
+}
+
 long ft_atol_assume_legit_input(char *str)
 {
     int i;
