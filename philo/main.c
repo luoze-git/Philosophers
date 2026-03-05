195c916dbf2d997a1118f91dd409a57ae7c0d6f0
luozguo <anitadou9@gmail.com>
2026-03-05 20:11:18 +0100

    refactor creation of structs

--- philo/main.c
+++ philo/main.c
@@ -1,22 +1,73 @@
 #include "philo.h"
 
+int detect_liveness(t_monitor *mona)
+{
+}
 
-// void start_simulation(t_control_philo *ctl);
-// {
+int update_eating_states(t_monitor *mona)
+{
+}
 
-// }
+int report_eater_state(t_monitor *mona, char *msg)
+{
+}
+
+void *eater_routine(void *arg)
+{
+    t_eater *philo;
+
+    philo = (t_eater *)arg;
+    while (mona->stop_flag != 1)
+    {
+        think(philo);
+        eat(philo);
+        sleep_philo(philo);
+    }
+    return (NULL);
+}
+
+int monitor_routine(t_monitor * mona)
+{
+
+}
+
+/*int pthread_create(
+    pthread_t *thread,
+    const pthread_attr_t *attr,
+    void *(*start_routine)(void *),
+    void *arg
+);
+*/
+
+void create_threads(t_monitor *mona)
+{
+    int i;
+    t_eater *eater;
+    i = 0;
+    eater = mona->eater;
+    while (i < mona->num_eater)
+    {
+        pthread_create(&eater[i].thread_id, NULL, eater_routine, &eater[i]);
+        i++;
+    }
+    pthread_create(&mona->monitor_thread, NULL, monitor_routine, mona);
+}
+
+/*control philo , philosss;  main born and recevie and cleanup , monitor death , philo eaters*/
+void start_simulation(t_monitor *mona);
+{
+    create_threads(mona);
+}
 
 // /// args could be 5 - 6; must check the type
 int main(int argc, char **argv)
 {
-    t_control_philo ctl;
-    if (parse_args(&ctl, argc, argv))
+    t_monitor mona;
+    if (parse_args(&mona, argc, argv))
         return (1);
-    if (init_philos_struct(&ctl))
+    if (prep_mona_n_eaters_pre_threads(&mona))
         return (1);
-
-    start_simulation(&ctl);
-
-    cleanup(&ctl);
+    start_simulation(&mona);
+    wait_to_cleanup_plates(&mona);
     return (0);
 }
