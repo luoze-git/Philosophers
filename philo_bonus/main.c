644f4805bf4c6ac23db35180880cc9cef5e71a5d
luozguo <anitadou9@gmail.com>
2026-03-10 19:16:12 +0100

    rough flow set tup

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -1,35 +1,74 @@
 #include "philo_bonus.h"
-typedef struct s_parent
-{
-    int num_eater;
-    long time_to_die;
-    long time_to_eat;
-    long time_to_sleep;
-    int must_eat_count;
-    int finished_eater;
-    int stop_flag;
-    long start_time_abs;
-} t_parent;
 
-int init_mom(t_parent *mom)
+
+// todo: you will check all the open failed detection. Clean strategy. 
+int setup_semaphore(t_parent *mama)
 {
-    mom->finished_eater = 0;
-    mom->stop_flag = 0;
-    mom->start_time_abs = get_current_absolute_time_in_ms();
+    sem_unlink ("\fork");
+    sem_unlink("\printf");
+    sem_unlink("\waiter");
+    mama->sem_forks = sem_open ("\fork" , O_CREAT, 0644, mama->num_eater);
+    mama->sem_printf = sem_open("\printf" ,O_CREAT, 0644 , 1);
+    mama->sem_waiter = sem_open ("\waiter", O_CREAT, 0644, mama->num_eater - 1);
     return 0;
 }
 
-int generate_processes(t_parent* mom)
+int init_mama(t_parent *mama)
 {
-    
+    if (setup_semaphore(mama))
+        return 1 ; 
+    mama->finished_eater = 0;
+    mama->start_time_abs = get_curr_time_absolute_in_ms();
+    // here no need to clean anything
+    mama->eater_pid = malloc(mama->num_eater * sizeof(*(mama->eater_pid)));
+    if (mama->eater_pid == NULL)
+        return 1;
+    return 0;
 }
+
+int start_simulation(t_parent* mama)
+{
+    int i;
+    i = 0;
+    while ( i < mama->num_eater)
+    {
+        mama->eater_pid[i] = fork();
+        if (mama->eater_pid[i] == 0)
+            eater_transform(mama, i + 1);
+        if (mama->eater_pid[i] == -1)
+            deal_with_mallocc_clean_n_pid(mama);
+        i++;
+    }
+    return 0;
+}
+
+wait_for_return(t_parent *mama)
+{
+    int i;
+    int returnd_pid;
+    int status;
+    i = 0;
+
+    // this is the loop for must_eat_count
+    while (i < mama->num_eater)
+    {
+        returnd_pid = waitpid(-1, &status, 0);
+        if (status == 1)
+            send_died_stop_signal(mama);
+        i++;
+    }
+
+}
+
 int main(int argc, char* argv[])
 {
-    t_parent mom;
-    if (parse_args(&mom , argc , argv))
+    t_parent mama;
+    if (parse_args(&mama , argc , argv))
         return 1;
-    init_mom(&mom);
-    generate_processes(&mom);
-    wait_for_return(&mom);
+    if (init_mama(&mama))
+        return 1 ;
+    if (start_simulation(&mama))
+        return 1; 
+    wait_for_return(&mama);
     return 0 ;
 }
