c0f9d2d70a971ada1a28f11194c2f91cb0434a8c
luozguo <anitadou9@gmail.com>
2026-03-10 19:16:31 +0100

    parse adapted to bonus ver

--- philo_bonus/parse.c
+++ philo_bonus/parse.c
@@ -75,41 +75,41 @@ int check_args_legitimacy(int argc, char **argv)
 ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
 */
 
-static void load_parsed_args(t_parent *mom, int argc, char **argv)
+static void load_parsed_args(t_parent *mama, int argc, char **argv)
 {
-	mom->num_eater = (int)ft_atol_assume_legit_input(argv[1]);
-	mom->time_to_die = ft_atol_assume_legit_input(argv[2]);
-	mom->time_to_eat = ft_atol_assume_legit_input(argv[3]);
-	mom->time_to_sleep = ft_atol_assume_legit_input(argv[4]);
+	mama->num_eater = (int)ft_atol_assume_legit_input(argv[1]);
+	mama->time_to_die = ft_atol_assume_legit_input(argv[2]);
+	mama->time_to_eat = ft_atol_assume_legit_input(argv[3]);
+	mama->time_to_sleep = ft_atol_assume_legit_input(argv[4]);
 	if (argc == 6)
-		mom->must_eat_count = (int)ft_atol_assume_legit_input(argv[5]);
+		mama->must_eat_count = (int)ft_atol_assume_legit_input(argv[5]);
 	else
-		mom->must_eat_count = -1;
+		mama->must_eat_count = -1;
 }
 
-int pre_terminate(t_parent *mom)
+int pre_terminate(t_parent *mama)
 {
-	if (mom->must_eat_count == 0)
+	if (mama->must_eat_count == 0)
 	{
 		write(1, "0 meals", 7);
 		return (1);
 	}
 	return (0);
 }
-/// @brief Precheck input satisfy the format. Then load the args into mom.
-/// @param mom
+/// @brief Precheck input satisfy the format. Then load the args into mama.
+/// @param mama
 /// @param argc
 /// @param argv
 /// @return 0 if suc. 1 if failed.
-int parse_args(t_parent *mom, int argc, char **argv)
+int parse_args(t_parent *mama, int argc, char **argv)
 {
 	if (check_args_legitimacy(argc, argv))
 	{
 		printf("this is not acceptible for me for a reason stated above.");
 		return (1);
 	}
-	load_parsed_args(mom, argc, argv);
-	if (pre_terminate(mom))
+	load_parsed_args(mama, argc, argv);
+	if (pre_terminate(mama))
 		return (1);
 	return (0);
 }
