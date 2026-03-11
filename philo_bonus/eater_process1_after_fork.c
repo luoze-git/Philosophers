e9e0e353e945a1270fb1cd469d676594187bbc23
luoze <anitadou9@gmail.com>
2026-03-11 12:15:14 +0100

    move detection of must eat to monitor.

---
+++ philo_bonus/eater_process1_after_fork.c
@@ -0,0 +1,56 @@
+#include "philo_bonus.h"
+
+int start_eater_thread(t_parent *mama, t_eater *eater)
+{
+    if (pthread_create(&eater->thread_eater_eater, NULL, eater_routine, eater) != 0)
+        return 1;
+    return 0;
+}
+
+int start_monitoring(t_eater *eater)
+{
+    int i;
+
+    i = 0;
+    while (1)
+    {
+        if (eater_is_dead(eater, eater->ptr_mama->time_to_die))
+        {
+            print_death_n_set_stop_n_never_post_sem(eater, "died.");
+            return (DEAD);
+        }
+        if (eater->ptr_mama->must_eat_count != -1 && eater_is_full(eater))
+        {
+            set_stop_flag_with_mutex(eater);
+            return (EATER_FULL);
+        }
+        usleep(1000);
+    }
+}
+
+int init_eater(t_eater *eater , t_parent *mama)
+{
+    eater->ptr_mama = mama;
+    eater->last_eating_time_abs = eater->ptr_mama->start_time_abs;
+    eater->meals_eaten = 0;
+    eater->stop_flag = 0 ;
+    if (pthread_mutex_init(&eater->mutex_eater, NULL))
+        return 1;
+    
+    return 0;
+}
+
+
+int eater_transform(t_parent *mama , int id_passed)
+{
+    t_eater eater;
+
+    eater.id = id_passed;
+    // this thread will remain as the monitor
+    if (init_eater(&eater, mama))
+        return (1);
+    if (start_eater_thread(mama , &eater))
+        return (1);
+    exit(start_monitoring(&eater));
+    return (0);
+}
---
+++ philo_bonus/eater_process2_eater_thread.c
@@ -0,0 +1,60 @@
+# include "philo_bonus.h"
+
+void lonely_eater(t_eater *eater)
+{
+    sem_wait(eater->ptr_mama->sem_forks);
+    print_live_state(eater, "has taken a fork");
+    sem_wait(eater->ptr_mama->sem_forks);
+}
+
+void eat(t_eater *eater)
+{
+    sem_wait(eater->ptr_mama->sem_waiter);
+    sem_wait(eater->ptr_mama->sem_forks);
+    print_live_state(eater, "has taken a fork");
+    sem_wait(eater->ptr_mama->sem_forks);
+    print_live_state(eater, "has taken a fork");
+
+    update_eating_state(eater);
+    print_live_state(eater, "is eating");
+
+    usleep(eater->ptr_mama->time_to_eat * 1000);
+    sem_post(eater->ptr_mama->sem_waiter);
+    sem_post(eater->ptr_mama->sem_forks);
+    sem_post(eater->ptr_mama->sem_forks);
+}
+
+void update_eating_state(t_eater *eater)
+{
+    pthread_mutex_lock(&eater->mutex_eater);
+    eater->last_eating_time_abs = get_curr_time_absolute_in_ms();
+    eater->meals_eaten++;
+    pthread_mutex_unlock(&eater->mutex_eater);
+}
+
+void sleep_eater(t_eater *eater)
+{
+    print_live_state(eater, "is sleeping");
+    usleep(eater->ptr_mama->time_to_sleep * 1000);
+}
+
+/*usleep() take microseconds*/
+/*tothink: why putting half to sleep is needed in the beginnning to prevent instant deadlock?*/
+void *eater_routine(void *arg)
+{
+    t_eater *eater;
+
+    eater = (t_eater *)arg;
+    if (eater->ptr_mama->num_eater > 1)
+    {
+        while (!stop_simulation_by_reading_stop_flag(eater))
+        {
+            eat(eater);
+            sleep_eater(eater);
+            print_live_state(eater, "is thinking");
+        }
+    }
+    else
+        lonely_eater(eater);
+    return (NULL);
+}
