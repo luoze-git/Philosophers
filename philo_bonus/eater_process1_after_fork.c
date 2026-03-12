23a3e9f28f22989ddf2336594d19c127c6e4010d
luozguo <anitadou9@gmail.com>
2026-03-12 17:02:55 +0100

    eater1 is perfect now

--- philo_bonus/eater_process1_after_fork.c
+++ philo_bonus/eater_process1_after_fork.c
@@ -1,24 +1,47 @@
 #include "philo_bonus.h"
 
-int create_eater_routine_thread( t_eater *eater)
+static int start_monitoring(t_eater *eater);
+static int init_eater(t_eater *eater , t_parent *mama);
+static int create_eater_routine_thread( t_eater *eater);
+
+// this thread will remain as the monitor
+int eater_transform(t_parent *mama , int id_passed)
+{
+    t_eater eater;
+    int monitor_code;
+    
+    eater.id = id_passed;
+    if (init_eater(&eater, mama))
+        exit(ERR);
+    if (create_eater_routine_thread(&eater))
+    {
+        destroy_local_mutex(&eater);
+        exit(ERR);
+    }    
+    monitor_code = start_monitoring(&eater);
+    clean_children_process(&eater);
+    exit(monitor_code);
+}
+
+static int create_eater_routine_thread(t_eater *eater)
 {
     if (pthread_create(&eater->thread_eater_eater, NULL, eater_routine, eater) != 0)
         return 1;
     return 0;
 }
 
-int init_eater(t_eater *eater , t_parent *mama)
+static int init_eater(t_eater *eater, t_parent *mama)
 {
     eater->ptr_mama = mama;
     eater->last_eating_time_abs = eater->ptr_mama->start_time_abs;
     eater->meals_eaten = 0;
-    eater->stop_flag = 0 ;
+    eater->stop_flag = 0;
     if (pthread_mutex_init(&eater->mutex_eater, NULL))
         return 1;
     return 0;
 }
 
-int start_monitoring(t_eater *eater)
+static int start_monitoring(t_eater *eater)
 {
     int i;
 
@@ -37,29 +60,4 @@ int start_monitoring(t_eater *eater)
         }
         usleep(1000);
     }
-}
-
-static void clean_children_process(t_eater *eater)
-{
-    join_eater_routine_thread(eater);
-    pthread_mutex_destroy(&eater->mutex_eater);
-}
-
-// this thread will remain as the monitor
-int eater_transform(t_parent *mama , int id_passed)
-{
-    t_eater eater;
-    int monitor_code;
-    
-    eater.id = id_passed;
-    if (init_eater(&eater, mama))
-        exit(ERR);
-    if (create_eater_routine_thread(&eater))
-    {
-        pthread_mutex_destroy(&eater.mutex_eater);
-        exit(ERR);
-    }    
-    monitor_code = start_monitoring(&eater);
-    clean_children_process(&eater);
-    exit(monitor_code);
 }
