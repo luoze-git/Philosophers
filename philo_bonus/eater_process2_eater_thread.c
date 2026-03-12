22427e04977674fd49773a3e6ce6336596f2eeca
luozguo <anitadou9@gmail.com>
2026-03-12 17:05:33 +0100

    eater2 is perfect now

--- philo_bonus/eater_process2_eater_thread.c
+++ philo_bonus/eater_process2_eater_thread.c
@@ -1,42 +1,9 @@
 # include "philo_bonus.h"
 
-void lonely_eater(t_eater *eater)
-{
-    sem_wait(eater->ptr_mama->sem_forks);
-    print_live_state(eater, "has taken a fork");
-    sem_wait(eater->ptr_mama->sem_forks);
-}
-
-void eat(t_eater *eater)
-{
-    sem_wait(eater->ptr_mama->sem_waiter);
-    sem_wait(eater->ptr_mama->sem_forks);
-    print_live_state(eater, "has taken a fork");
-    sem_wait(eater->ptr_mama->sem_forks);
-    print_live_state(eater, "has taken a fork");
-
-    update_eating_state(eater);
-    print_live_state(eater, "is eating");
-
-    usleep(eater->ptr_mama->time_to_eat * 1000);
-    sem_post(eater->ptr_mama->sem_waiter);
-    sem_post(eater->ptr_mama->sem_forks);
-    sem_post(eater->ptr_mama->sem_forks);
-}
-
-void update_eating_state(t_eater *eater)
-{
-    pthread_mutex_lock(&eater->mutex_eater);
-    eater->last_eating_time_abs = get_curr_time_absolute_in_ms();
-    eater->meals_eaten++;
-    pthread_mutex_unlock(&eater->mutex_eater);
-}
-
-void sleep_eater(t_eater *eater)
-{
-    print_live_state(eater, "is sleeping");
-    usleep(eater->ptr_mama->time_to_sleep * 1000);
-}
+static void sleep_eater(t_eater *eater);
+static void update_eating_state(t_eater *eater);
+static void eat(t_eater *eater);
+static void lonely_eater(t_eater *eater);
 
 /*usleep() take microseconds*/
 void *eater_routine(void *arg)
@@ -56,4 +23,42 @@ void *eater_routine(void *arg)
     else
         lonely_eater(eater);
     return (NULL);
+}
+
+static void lonely_eater(t_eater *eater)
+{
+    sem_wait(eater->ptr_mama->sem_forks);
+    print_live_state(eater, "has taken a fork");
+    sem_wait(eater->ptr_mama->sem_forks);
+}
+
+static void eat(t_eater *eater)
+{
+    sem_wait(eater->ptr_mama->sem_waiter);
+    sem_wait(eater->ptr_mama->sem_forks);
+    print_live_state(eater, "has taken a fork");
+    sem_wait(eater->ptr_mama->sem_forks);
+    print_live_state(eater, "has taken a fork");
+
+    update_eating_state(eater);
+    print_live_state(eater, "is eating");
+
+    usleep(eater->ptr_mama->time_to_eat * 1000);
+    sem_post(eater->ptr_mama->sem_waiter);
+    sem_post(eater->ptr_mama->sem_forks);
+    sem_post(eater->ptr_mama->sem_forks);
+}
+
+static void update_eating_state(t_eater *eater)
+{
+    pthread_mutex_lock(&eater->mutex_eater);
+    eater->last_eating_time_abs = get_curr_time_absolute_in_ms();
+    eater->meals_eaten++;
+    pthread_mutex_unlock(&eater->mutex_eater);
+}
+
+static void sleep_eater(t_eater *eater)
+{
+    print_live_state(eater, "is sleeping");
+    usleep(eater->ptr_mama->time_to_sleep * 1000);
 }
